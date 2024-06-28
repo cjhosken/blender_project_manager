2b6ab1007e8b0d61c51230b269f3c16ce36a035a
Christopher Hosken <hoskenchristopher@gmail.com>
2024-06-28 18:02:04 +0100

    update for blender 4.2

--- __init__.py
+++ __init__.py
@@ -1,18 +1,3 @@
-#  Copyright 2022 Christopher Hosken
-# 
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 import bpy, os
 from bpy.props import StringProperty
 from bpy.types import Operator, Panel
@@ -27,7 +12,7 @@ bl_info = {
     "name" : "Project Manager",
     "author" : "Christopher Hosken",
     "version" : (1, 0, 0),
-    "blender" : (4, 1, 0),
+    "blender" : (4, 2, 0),
     "description" : "",
     "warning" : "",
     "support": "COMMUNITY",
